aa9f6a435cacdb9d642a5ca04011f731444b6b05
Manish Bhatt <manish.bhatt@lji.io>
2022-02-20 21:13:44 +0530

    implemented React context API

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Stateprovider } from './StateProvider';
+import reducer, {initialState} from './reducer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Stateprovider initialState={initialState} reducer={reducer}>
+      <App />
+    </Stateprovider>
   </React.StrictMode>,
   document.getElementById('root')
 );
